a7ce87df758f42529d8eaf03e3c8016f85fb85f0
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-10-12 23:51:32 +0500

    format some files by prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,41 @@
 import type { Metadata } from 'next';
-import { Inter, Calistoga } from 'next/font/google';
-import './globals.css';
+import { Calistoga, Inter } from 'next/font/google';
 import { twMerge } from 'tailwind-merge';
 
+import './globals.css';
+
 const inter = Inter({
-	subsets: ['latin'],
-	variable: '--font-sans',
+  subsets: ['latin'],
+  variable: '--font-sans',
 });
 
 const calistoga = Calistoga({
-	subsets: ['latin'],
-	variable: '--font-serif',
-	weight: '400',
+  subsets: ['latin'],
+  variable: '--font-serif',
+  weight: '400',
 });
 
 export const metadata: Metadata = {
-	title: 'My Portfolio',
-	description: 'Created with the help of Frontend Tribe',
+  title: 'My Portfolio',
+  description: 'Created with the help of Frontend Tribe',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body className={twMerge(inter.variable, calistoga.variable, 'bg-gray-900 text-white antialiased font-sans')}>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          'bg-gray-900 font-sans text-white antialiased',
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,22 +1,22 @@
 export const Header = () => {
-	return (
-		<div>
-			<nav>
-				<ul>
-					<li>
-						<a href='#'>Home</a>
-					</li>
-					<li>
-						<a href='#'>Projects</a>
-					</li>
-					<li>
-						<a href='#'>About</a>
-					</li>
-					<li>
-						<a href='#'>Contact</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	);
+  return (
+    <div className="relative top-3 flex items-center justify-center">
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
