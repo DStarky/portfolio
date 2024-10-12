0190d400adf9eb97ac738aa5697e85288b38a4a9
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-10-13 00:30:29 +0500

    add Header component

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,18 +2,38 @@ export const Header = () => {
   return (
     <div className="relative top-3 flex items-center justify-center">
       <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
+        <ul className="flex gap-1 rounded-full border border-white/15 bg-white/10 p-0.5 backdrop-blur">
+          <li className="nav-item">
+            <a
+              href="#"
+              className="nav-link"
+            >
+              Home
+            </a>
           </li>
-          <li>
-            <a href="#">Projects</a>
+          <li className="nav-item">
+            <a
+              href="#"
+              className="nav-link"
+            >
+              Projects
+            </a>
           </li>
-          <li>
-            <a href="#">About</a>
+          <li className="nav-item">
+            <a
+              href="#"
+              className="nav-link"
+            >
+              About
+            </a>
           </li>
-          <li>
-            <a href="#">Contact</a>
+          <li className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
+            <a
+              href="#"
+              className="nav-link"
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
