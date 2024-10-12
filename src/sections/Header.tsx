23c2d44430ee99e80fba1b71653fa9e41154abe8
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-10-12 23:31:52 +0500

    added a basic style for layout

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,22 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+	return (
+		<div>
+			<nav>
+				<ul>
+					<li>
+						<a href='#'>Home</a>
+					</li>
+					<li>
+						<a href='#'>Projects</a>
+					</li>
+					<li>
+						<a href='#'>About</a>
+					</li>
+					<li>
+						<a href='#'>Contact</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
 };
