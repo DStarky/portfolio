978f9cfbac291e531baa4e66b2062a9befbe8271
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-10-24 10:54:35 +0500

    updated Project section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import SectionHeader from '@/components/SectionHeader';
+
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 import CheckCircleIcon from '@/assets/icons/check-circle.svg';
 import aiStartupLandingPage from '@/assets/images/ai-startup-landing-page.png';
@@ -50,17 +52,11 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <div className="flex justify-center">
-          <p className="bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-center font-semibold uppercase tracking-widest text-transparent">
-            Real-world Results
-          </p>
-        </div>
-        <h2 className="mt-6 text-center font-serif text-3xl md:text-5xl">
-          Featured Projects
-        </h2>
-        <p className="mx-auto mt-4 max-w-md text-center text-white/60 md:text-lg lg:text-xl">
-          See how I transformed concepts into engaging digital experrience.
-        </p>
+        <SectionHeader
+          eyebrow="Real-world Results"
+          title="Featured Projects"
+          description="See how I transformed concepts into engaging digital experrience."
+        />
         <div className="mt-10 flex flex-col gap-20 md:mt-20">
           {portfolioProjects.map(project => (
             <div
