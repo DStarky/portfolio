d674bf7883397c6987c57f6e6f075dc52ae1dffd
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-10-29 15:52:37 +0500

    added card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,27 @@
-const Card = () => {
+import { PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import grainImage from '@/assets/images/grain.jpg';
+
+const Card = ({
+  className = '',
+  children,
+}: PropsWithChildren<{ className?: string }>) => {
   return (
-    <div>Card</div>
-  )
-}
-export default Card
+    <div
+      className={twMerge(
+        "relative z-0 overflow-hidden rounded-3xl bg-gray-800 p-6 after:pointer-events-none after:absolute after:inset-0 after:z-10 after:rounded-3xl after:outline after:outline-2 after:-outline-offset-2 after:outline-white/20 after:content-['']",
+        className,
+      )}
+    >
+      <div
+        className="absolute inset-0 -z-10 opacity-5"
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      ></div>
+      {children}
+    </div>
+  );
+};
+export default Card;
