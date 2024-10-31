a0be3a148484e5e9ce2b805f075c445c4c4494a7
Dmitrii Starkov <starkoffbusiness@gmail.com>
2024-10-31 14:23:02 +0500

    finished testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -45,7 +45,7 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 lg:py-24">
       <div className="container">
         <SectionHeader
           eyebrow="Happy Clients"
@@ -53,27 +53,34 @@ export const TestimonialsSection = () => {
           description="Don`t just take my word for it. See what my clients have to say about my
         work."
         />
-        <div>
-          {testimonials.map(testimonial => (
-            <Card key={testimonial.name}>
-              <div className="flex items-center gap-4">
-                <div className="inline-flex size-14 items-center justify-center rounded-full bg-gray-700">
-                  <Image
-                    src={testimonial.avatar}
-                    alt={testimonial.name}
-                    className="max-h-full"
-                  />
-                </div>
-                <div>
-                  <div className="font-semibold">{testimonial.name}</div>
-                  <div className="text-sm text-white/40">
-                    {testimonial.position}
+        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] lg:mt-20">
+          <div className="flex flex-none gap-8">
+            {testimonials.map(testimonial => (
+              <Card
+                key={testimonial.name}
+                className="max-w-xs md:max-w-md md:p-8"
+              >
+                <div className="flex items-center gap-4">
+                  <div className="inline-flex size-14 flex-shrink-0 items-center justify-center rounded-full bg-gray-700">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <p className="mt-4 text-sm">{testimonial.text}</p>
-            </Card>
-          ))}
+                <p className="mt-4 text-sm md:mt-6 md:text-base">
+                  {testimonial.text}
+                </p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
